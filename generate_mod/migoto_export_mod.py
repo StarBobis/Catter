81930aebf8d337937d2ee0fa99a38e64e195f1ab
NicoMico <Administrator@DESKTOP>
2024-12-20 09:42:43 +0800

    op

--- generate_mod/migoto_export_mod.py
+++ generate_mod/migoto_export_mod.py
@@ -24,29 +24,50 @@ from .m_drawib_model import *
 class ModModel:
     drawib_drawibmodel_dict:dict[str,DrawIBModel] = {}
     shapekeys = {}
+    
+    global_key_index_constants = 0
+    global_key_index_logic = 0
+    global_generate_mod_number = 0
 
     @classmethod
     def initialzie(cls):
+        '''
+        You have to call this to clean cache data before generate mod.
+        '''
         cls.drawib_drawibmodel_dict = {}
         cls.shapekeys = {}
+        cls.global_key_index_constants = 0
+        cls.global_key_index_logic = 0
+        cls.global_generate_mod_number = 0
 
     @classmethod
     def get_style_alias(cls,partname:str):
         '''
-        把当前PartName转为GIMI风格的命名，因为用户已经习惯了这种命名风格
-        具体哪种风格还是要看DBMT里的配置
+        Convert to GIMI name style because it's widely used by Mod author.
         '''
         partname_gimi_alias_dict = {
             "1":"Head","2":"Body","3":"Dress","4":"Extra"
             ,"5":"Extra1","6":"Extra2","7":"Extra3","8":"Extra4","9":"Extra5"
             ,"10":"Extra6","11":"Extra7","12":"Extra8"}
         return partname_gimi_alias_dict.get(partname,partname)
+    
+    @classmethod
+    def get_mod_switch_key(cls,key_index:int):
+        '''
+        Default mod switch/toggle key.
+        '''
+        key_list = ["x","c","v","b","n","m","j","k","l","o","p","[","]",
+                    "x","c","v","b","n","m","j","k","l","o","p","[","]",
+                    "x","c","v","b","n","m","j","k","l","o","p","[","]"]
+        return key_list[key_index]
 
     @classmethod
     def export_buffer_files(cls):
-        # 输出IB和Category Buffer文件
+        '''
+        Export to buffer files from ib and vb.
+        '''
         for draw_ib, draw_ib_model in cls.drawib_drawibmodel_dict.items():
-            # 输出各个IB文件
+            # Export IndexBuffer files.
             for partname in draw_ib_model.part_name_list:
                 component_name = "Component " + partname
                 ib_buf = draw_ib_model.componentname_ibbuf_dict.get(component_name,None)
@@ -58,29 +79,22 @@ class ModModel:
                         for ib_byte_number in ib_buf:
                             ibf.write(ib_byte_number) 
 
-            # 输出各个Category的Buffer到文件
+            # Export category buffer files.
             for category_name, category_buf in draw_ib_model.categoryname_bytelist_dict.items():
                 buf_path = MainConfig.path_generatemod_buffer_folder(draw_ib=draw_ib) + draw_ib + "-" + category_name + ".buf"
                 buf_bytearray = bytearray(category_buf)
                 with open(buf_path, 'wb') as ibf:
                     ibf.write(buf_bytearray)
         
-        # 如果shapekey存在的话，输出shapekey (WWMI会用到)
-        
-    @classmethod
-    def get_mod_switch_key(cls,key_index:int):
-        key_list = ["x","c","v","b","n","m","j","k","l","o","p","[","]",
-                    "x","c","v","b","n","m","j","k","l","o","p","[","]",
-                    "x","c","v","b","n","m","j","k","l","o","p","[","]"]
-        return key_list[key_index]
+        # TODO WWMI need to output shapekey buffers.
+        if len(cls.shapekeys) != 0:
+            pass
 
     @classmethod
     def generate_unity_vs_config_ini(cls):
         ini_builder = M_IniBuilder()     
 
-        global_key_index_constants = 0
-        global_key_index_logic = 0
-        generate_mod_number = 0
+
 
         for draw_ib, draw_ib_model in cls.drawib_drawibmodel_dict.items():
             d3d11GameType = draw_ib_model.d3d11GameType
@@ -89,9 +103,9 @@ class ModModel:
                 # 声明Constants变量
                 constants_section = M_IniSection(M_SectionType.Constants)
                 constants_section.append("[Constants]")
-                constants_section.append("global $active" + str(generate_mod_number))
+                constants_section.append("global $active" + str(cls.global_generate_mod_number))
                 for i in range(draw_ib_model.key_number):
-                    key_str = "global persist $swapkey" + str(i + global_key_index_constants) + " = 0"
+                    key_str = "global persist $swapkey" + str(i + cls.global_key_index_constants) + " = 0"
                     constants_section.append(key_str) 
                 constants_section.new_line()
 
@@ -100,7 +114,7 @@ class ModModel:
                 # 声明$active激活变量
                 present_section = M_IniSection(M_SectionType.Present)
                 present_section.append("[Present]")
-                present_section.append("post $active" + str(generate_mod_number) + " = 0")
+                present_section.append("post $active" + str(cls.global_generate_mod_number) + " = 0")
                 present_section.new_line()
 
                 ini_builder.append_section(present_section)
@@ -118,9 +132,9 @@ class ModModel:
 
                     if toggle_type_number >= 2:
                         key_section = M_IniSection(M_SectionType.Key)
-                        key_section.append("[KeySwap" + str(global_key_index_constants) + "]")
-                        key_section.append("condition = $active" + str(generate_mod_number) + " == 1")
-                        key_section.append("key = " + cls.get_mod_switch_key(global_key_index_constants))
+                        key_section.append("[KeySwap" + str(cls.global_key_index_constants) + "]")
+                        key_section.append("condition = $active" + str(cls.global_generate_mod_number) + " == 1")
+                        key_section.append("key = " + cls.get_mod_switch_key(cls.global_key_index_constants))
                         key_section.append("type = cycle")
                         
                         key_cycle_str = ""
@@ -130,26 +144,26 @@ class ModModel:
                             else:
                                 key_cycle_str = key_cycle_str + str(i)
 
-                        key_section.append("$swapkey" + str(global_key_index_constants) + " = " + key_cycle_str)
+                        key_section.append("$swapkey" + str(cls.global_key_index_constants) + " = " + key_cycle_str)
                         key_section.new_line()
 
                         ini_builder.append_section(key_section)
-                        global_key_index_constants = global_key_index_constants + 1
+                        cls.global_key_index_constants = cls.global_key_index_constants + 1
                     
                     if switch_type_number >= 1:
                         for i in range(switch_type_number):
                             key_section = M_IniSection(M_SectionType.Key)
-                            key_section.append("[KeySwap" + str(global_key_index_constants) + "]")
-                            key_section.append("condition = $active" + str(generate_mod_number) + " == 1")
-                            key_section.append("key = " + cls.get_mod_switch_key(global_key_index_constants))
+                            key_section.append("[KeySwap" + str(cls.global_key_index_constants) + "]")
+                            key_section.append("condition = $active" + str(cls.global_generate_mod_number) + " == 1")
+                            key_section.append("key = " + cls.get_mod_switch_key(cls.global_key_index_constants))
                             key_section.append("type = cycle")
-                            key_section.append("$swapkey" + str(global_key_index_constants) + " = 1,0")
+                            key_section.append("$swapkey" + str(cls.global_key_index_constants) + " = 1,0")
                             key_section.new_line()
 
                             ini_builder.append_section(key_section)
-                            global_key_index_constants = global_key_index_constants + 1
+                            cls.global_key_index_constants = cls.global_key_index_constants + 1
             
-            vertex_limit_raise_filter_index = cls.add_vertex_limit_raise_section(ini_builder=ini_builder,draw_ib_model=draw_ib_model,generate_mod_number=generate_mod_number)
+            vertex_limit_raise_filter_index = cls.add_vertex_limit_raise_section(ini_builder=ini_builder,draw_ib_model=draw_ib_model,generate_mod_number=cls.global_generate_mod_number)
             
             # 声明TextureOverrideVB部分，只有使用GPU-PreSkinning时是直接替换hash对应槽位
             if d3d11GameType.GPU_PreSkinning:
@@ -171,7 +185,7 @@ class ModModel:
                     if category_name == d3d11GameType.CategoryDrawCategoryDict["Texcoord"]:
                         if vertex_limit_raise_filter_index:
                             filterindex_indent_prefix = "  "
-                            texture_override_vb_section.append("if vb0 == " + str(3000 + generate_mod_number))
+                            texture_override_vb_section.append("if vb0 == " + str(3000 + cls.global_generate_mod_number))
 
                     # 遍历获取所有在当前分类hash下进行替换的分类，并添加对应的资源替换
                     for original_category_name, draw_category_name in d3d11GameType.CategoryDrawCategoryDict.items():
@@ -197,7 +211,7 @@ class ModModel:
                     # 分支架构，如果是Position则需提供激活变量
                     if category_name == d3d11GameType.CategoryDrawCategoryDict["Position"]:
                         if draw_ib_model.key_number != 0:
-                            texture_override_vb_section.append("$active" + str(generate_mod_number) + " = 1")
+                            texture_override_vb_section.append("$active" + str(cls.global_generate_mod_number) + " = 1")
 
                     texture_override_vb_section.new_line()
 
@@ -222,7 +236,7 @@ class ModModel:
                     vlr_filterindex_indent = "  "
                 
                 if vertex_limit_raise_filter_index:
-                    texture_override_ib_section.append("if vb0 == " + str(3000 + generate_mod_number))
+                    texture_override_ib_section.append("if vb0 == " + str(3000 + cls.global_generate_mod_number))
 
                 texture_override_ib_section.append(vlr_filterindex_indent + "handling = skip")
 
@@ -280,9 +294,9 @@ class ModModel:
                 if toggle_type_number >= 2:
                     for toggle_count in range(toggle_type_number):
                         if toggle_count == 0:
-                            texture_override_ib_section.append(vlr_filterindex_indent + "if $swapkey" + str(global_key_index_logic) + " == " + toggle_count)
+                            texture_override_ib_section.append(vlr_filterindex_indent + "if $swapkey" + str(cls.global_key_index_logic) + " == " + toggle_count)
                         else:
-                            texture_override_ib_section.append(vlr_filterindex_indent + "else if $swapkey" + str(global_key_index_logic) + " == " + toggle_count)
+                            texture_override_ib_section.append(vlr_filterindex_indent + "else if $swapkey" + str(cls.global_key_index_logic) + " == " + toggle_count)
 
                         toggle_model_collection = toggle_model_collection_list[toggle_count]
                         for obj_name in toggle_model_collection.obj_name_list:
@@ -293,7 +307,7 @@ class ModModel:
                     texture_override_ib_section.append("endif")
                     texture_override_ib_section.new_line()
 
-                    global_key_index_logic = global_key_index_logic + 1
+                    cls.global_key_index_logic = cls.global_key_index_logic + 1
                 elif toggle_type_number != 0:
                     for toggle_model_collection in toggle_model_collection_list:
                         for obj_name in toggle_model_collection.obj_name_list:
@@ -304,7 +318,7 @@ class ModModel:
 
                 # 输出按键开关的DrawIndexed
                 for switch_model_collection in switch_model_collection_list:
-                    texture_override_ib_section.append(vlr_filterindex_indent + "if $swapkey" + str(global_key_index_logic) + "  == 1")
+                    texture_override_ib_section.append(vlr_filterindex_indent + "if $swapkey" + str(cls.global_key_index_logic) + "  == 1")
                     for obj_name in switch_model_collection.obj_name_list:
                         m_drawindexed = draw_ib_model.obj_name_drawindexed_dict[obj_name]
                         texture_override_ib_section.append(vlr_filterindex_indent + "; " + m_drawindexed.AliasName)
@@ -312,7 +326,7 @@ class ModModel:
                         texture_override_ib_section.new_line()
                     texture_override_ib_section.append(vlr_filterindex_indent + "endif")
                     texture_override_ib_section.new_line()
-                    global_key_index_logic = global_key_index_logic + 1
+                    cls.global_key_index_logic = cls.global_key_index_logic + 1
                 
                 if vlr_filterindex_indent:
                     texture_override_ib_section.append("endif")
@@ -325,7 +339,7 @@ class ModModel:
             cls.add_resource_texture_sections(ini_builder=ini_builder,draw_ib_model=draw_ib_model)
             cls.move_slot_style_textures(draw_ib_model=draw_ib_model)
                 
-            generate_mod_number = generate_mod_number + 1
+            cls.global_generate_mod_number = cls.global_generate_mod_number + 1
 
             if GenerateModConfig.generate_to_seperate_folder():
                 draw_ib_output_folder = MainConfig.path_generate_mod_folder() + draw_ib + "\\"
@@ -334,9 +348,7 @@ class ModModel:
                 ini_builder.save_to_file(draw_ib_output_folder + "Config.ini")
                 ini_builder.ini_section_list.clear()
                 ini_builder.line_list.clear()
-            
-        # TODO 添加Hash方式的贴图ini生成方法
-        
+
         cls.generate_hash_style_texture_ini()
 
         if not GenerateModConfig.generate_to_seperate_folder():
