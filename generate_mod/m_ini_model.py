39b8ab21ddd2a9ad2314f548cc6eb5d83338d5c5
NicoMico <Administrator@DESKTOP>
2024-12-26 18:33:32 +0800

    op

--- generate_mod/m_ini_model.py
+++ generate_mod/m_ini_model.py
@@ -461,6 +461,7 @@ class M_IniModel:
                 resource_and_textureoverride_texture_section.new_line()
 
                 resource_and_textureoverride_texture_section.append("[TextureOverride_" + texture_hash + "]")
+                resource_and_textureoverride_texture_section.append("; " + new_texture_file_name)
                 resource_and_textureoverride_texture_section.append("hash = " + texture_hash)
                 resource_and_textureoverride_texture_section.append("this = Resource_Texture_" + texture_hash)
                 resource_and_textureoverride_texture_section.new_line()
