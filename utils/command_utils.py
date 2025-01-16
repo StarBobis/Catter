f4194260c4d26d1199fdb6c87184cebca7054b20
NicoMico <Administrator@DESKTOP>
2025-01-16 17:47:14 +0800

    op

--- utils/command_utils.py
+++ utils/command_utils.py
@@ -13,10 +13,11 @@ class CommandUtils:
         '''
         if GenerateModConfig.open_generated_mod_folder_after_run():
             generated_mod_folder_path = MainConfig.path_generate_mod_folder()
-            if " " in generated_mod_folder_path:
-                generated_mod_folder_path = '"{}"'.format(generated_mod_folder_path)
+            # if " " in generated_mod_folder_path:
+            #     generated_mod_folder_path = '"{}"'.format(generated_mod_folder_path)
             # print("generated_mod_folder_path: " + generated_mod_folder_path)
-            subprocess.run(['explorer',generated_mod_folder_path])
+            # subprocess.run(['explorer',generated_mod_folder_path])
+            os.startfile(generated_mod_folder_path)
 
 
 
