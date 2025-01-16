95b88e020697afbd0d73f1107193e12f7b27acfa
NicoMico <Administrator@DESKTOP>
2025-01-16 17:49:05 +0800

    op

--- utils/command_utils.py
+++ utils/command_utils.py
@@ -13,6 +13,8 @@ class CommandUtils:
         '''
         if GenerateModConfig.open_generated_mod_folder_after_run():
             generated_mod_folder_path = MainConfig.path_generate_mod_folder()
+
+            # XXX 不能使用subprocess.run('explorer',path)的方式打开文件夹，否则部分用户的电脑上无法识别到路径，且自动打开 文档 文件夹。
             # if " " in generated_mod_folder_path:
             #     generated_mod_folder_path = '"{}"'.format(generated_mod_folder_path)
             # print("generated_mod_folder_path: " + generated_mod_folder_path)
