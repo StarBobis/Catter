5a60b3d51f3bfcfbaddf231e30cdd9fe1b17221a
NicoMico <Administrator@DESKTOP>
2025-01-16 17:39:58 +0800

    op

--- utils/command_utils.py
+++ utils/command_utils.py
@@ -12,8 +12,10 @@ class CommandUtils:
         This will be call after generate mod, it will open explorer and shows the result mod files generated.
         '''
         if GenerateModConfig.open_generated_mod_folder_after_run():
-            generated_mod_folder_path = "'" + MainConfig.path_generate_mod_folder() + "'"
-            print("generated_mod_folder_path: " + generated_mod_folder_path)
+            generated_mod_folder_path = MainConfig.path_generate_mod_folder()
+            if " " in generated_mod_folder_path:
+                generated_mod_folder_path = '"{}"'.format(generated_mod_folder_path)
+            # print("generated_mod_folder_path: " + generated_mod_folder_path)
             subprocess.run(['explorer',generated_mod_folder_path])
 
 
