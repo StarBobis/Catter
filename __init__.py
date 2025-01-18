79bff3776cdf481a188054e3cc1f08dc571cd887
NicoMico <Administrator@DESKTOP>
2025-01-18 18:29:33 +0800

    op

--- __init__.py
+++ __init__.py
@@ -29,7 +29,7 @@ bl_info = {
     "name": "Catter",
     "description": "A blender plugin for game mod with 3Dmigoto.",
     "blender": (3, 6, 0),
-    "version": (1, 0, 8, 2),
+    "version": (1, 0, 8, 3),
     "location": "View3D",
     "category": "Generic",
     "tracker_url":"https://github.com/StarBobis/Catter"
